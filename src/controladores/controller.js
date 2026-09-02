94295552d0b6a6471a627dd8173807d53719f487
Adriana <adrianasc0230@gmail.com>
2026-09-01 19:36:39 -0500

    leer maestro por ID mostrando todos los datos

--- src/controladores/controller.js
+++ src/controladores/controller.js
@@ -12,7 +12,7 @@ try{
     if(guardarMaestro._id){
         res.json({
             mensaje: "Maestro Guardado",
-            datos: guardarMaestro._id
+            datos: guardarMaestro
 
         });
         
@@ -20,6 +20,7 @@ try{
     
 }
 catch(error){
+    console.log(error);
     res.json({
         mensaje: "Maestro No Guardado",
         datos: error
